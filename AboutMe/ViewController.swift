49a3cb51f0df3893ba06eaf962af62d97d1b5141
Mitchell Budge <mitchellbudge@gmail.com>
2019-04-23 10:43:54 -0600

    The app prints my name to the console window

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("My name is Mitchell Budge")
+        print("Hello #2")
     }
 
 
